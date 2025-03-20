c6104f447c56197f8c40a98680696cfc520e8d0c
unknown <evallardy@gmail.com>
2025-03-20 13:42:40 -0600

    spotify 4

--- musica/views.py
+++ musica/views.py
@@ -80,7 +80,7 @@ def cambiar_estatus_cancion(request, id):
 
         sp = spotipy.Spotify(auth=token)
 
-        playlist_id = 'https://open.spotify.com/playlist/729CSyaetSSlBtxbh2dBOp?si=0988f8a53ca44c78'
+        playlist_id = 'https://open.spotify.com/playlist/729CSyaetSSlBtxbh2dBOp?si=ca6a037a73224ad5'
         
         try:
             # ✅ Pasar directamente el valor de la URI
