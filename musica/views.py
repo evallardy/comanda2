d9d36de403bd6c1d619fad51699e0be3a69bbac8
unknown <evallardy@gmail.com>
2025-03-22 22:26:22 -0600

    spotify 25

--- musica/views.py
+++ musica/views.py
@@ -50,7 +50,7 @@ client_id = settings.CLIENT_ID
 client_secret = settings.CLIENT_SECRET
 redirect_uri = settings.REDIRECT_URI
 
-scope = 'playlist-modify-public playlist-modify-private user-library-modify'
+scope = 'playlist-modify-public playlist-modify-private'
 
 sp_oauth = SpotifyOAuth(
     client_id=client_id,
